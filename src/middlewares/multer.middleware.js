47ea1068bf11678d11119957fb1281360c0dfe96
harshchaturvedi2906 <harsh@clearspot.ai>
2024-10-29 13:32:12 +0530

    multer and cloudinary setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+//multer is used to file upload tasks we can also use express-file-upload also instead of this
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
